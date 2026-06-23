b8ad41f22a63240e4c2cf01d8ec43b4b523d8fa9
anjukavya <142564555+anjukavya@users.noreply.github.com>
2026-06-23 17:22:12 +0530

    Fix SQL queries and add department selection

--- operatos1.sql
+++ operatos1.sql
@@ -1,4 +1,4 @@
-select * from emp  where sal>50000 and job="saleman or "Marketing";
+select * from emp  where sal>50000 and job="saleman" or "Marketing";
 select ename,(sal*1.10) as total_sal from emp;
 select * from emp where hiredate between '2018-01-01' and '2020-12-31';
 select * from emp where ename like 'a%' or ename like 'b%' order by ename asc;
